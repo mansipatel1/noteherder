c2cd7a7ecab832abfcfdf94eabc84bc5ef29e2e1
Mansi <mansigpatel1@gmail.com>
2018-05-23 14:07:30 -0400

    added aphrodite

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,12 +1,74 @@
-import React from 'react'
+import React, { Component } from 'react'
+import RichTextEditor from 'react-rte'
 
 import './NoteForm.css'
 
-const NoteForm = () => {
-    return (
+class NoteForm extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        note: this.blankNote(),
+        editorValue: RichTextEditor.createEmptyValue(),
+      }
+    }
+  
+    componentWillReceiveProps = (nextProps) => {
+      const idFromUrl = nextProps.match.params.id
+      const note = nextProps.notes[idFromUrl] || this.blankNote()
+  
+      const noteNotFound = idFromUrl && !note.id
+      if (noteNotFound && nextProps.firebaseNotesSynced) {
+        this.props.history.push('/notes')
+      }
+  
+      let editorValue = this.state.editorValue
+      if (editorValue.toString('html') !== note.body) {
+        editorValue = RichTextEditor.createValueFromString(note.body, 'html')
+      }
+  
+      this.setState({ note, editorValue })
+    }
+  
+    blankNote = () => {
+      return {
+        id: null,
+        title: '',
+        body: '',
+        updatedAt: null,
+      }
+    }
+  
+    handleChanges = (ev) => {
+      const note = {...this.state.note}
+      note[ev.target.name] = ev.target.value
+  
+      this.setState(
+        { note },
+        () => this.props.saveNote(note)
+      )
+    }
+  
+    handleEditorChanges = (editorValue) => {
+      const note = {...this.state.note}
+      note.body = editorValue.toString('html')
+      this.setState(
+        { note, editorValue },
+        () => this.props.saveNote(note)
+      )
+    }
+  
+    handleRemove = () => {
+      this.props.removeNote(this.state.note)
+    }
+  
+    render() {
+      return (
         <div className="NoteForm">
           <div className="form-actions">
-            <button type="button">
+            <button
+              type="button"
+              onClick={this.handleRemove}
+            >
               <i className="fa fa-trash-o"></i>
             </button>
           </div>
@@ -16,13 +78,21 @@ const NoteForm = () => {
                 type="text"
                 name="title"
                 placeholder="Title your note"
+                value={this.state.note.title}
+                onChange={this.handleChanges}
               />
             </p>
             
-            <textarea name="body"></textarea>
+            <RichTextEditor
+              name="body"
+              placeholder="Just start typing..."
+              value={this.state.editorValue}
+              onChange={this.handleEditorChanges}
+            ></RichTextEditor>
           </form>
         </div>
-    )
-}
-
-export default NoteForm
+      )
+    }
+  }
+  
+  export default NoteForm
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react'
-
-import './Sidebar.css'
+import { StyleSheet, css } from 'aphrodite'
 
 import quill from './quill.svg'
 import newIcon from './new.png'
@@ -8,31 +7,45 @@ import newHover from './new-hover.png'
 
 const Sidebar = () => {
   return (
-    <nav
-      className="Sidebar"
-    >
+    <nav className={css(styles.sidebar)}>
       <div
-        className="logo"
+        className={css(styles.logo)}
       >
         <img
           src={quill}
           alt="Noteherder"
+          className={css(styles.logoImg)}
         />
       </div>
-      <a className="new-note" href="/notes">
-        <img src={newHover} alt="New note" />
-        <img className="outline" src={newIcon} alt="New note" />
+      <a
+        href="/notes"
+        className={css(styles.newNote)}
+      >
+        <img
+          src={newHover}
+          alt="New note"
+          className={css(styles.newNoteImg)}
+        />
+        <img
+          src={newIcon}
+          alt="New note"
+          className={css(styles.newNoteImg, styles.newNoteImgHover)}
+        />
       </a>
-      <div className="SignOut">
-        <button>
-          <i className="fa fa-sign-out"></i>
+      <div
+        className={css(styles.signOut)}
+      >
+        <button className={css(styles.button)}>
+          <i
+            className={`fa fa-sign-out ${css(styles.buttonIcon)}`}
+          ></i>
         </button>
       </div>
-    </nav> 
+    </nav>
   )
 }
 
-/*const styles = {
+const styles = StyleSheet.create({
   sidebar: {
     width: '6rem',
     backgroundColor: '#f3f3f3',
@@ -50,6 +63,36 @@ const Sidebar = () => {
     width: '3rem',
     paddingLeft: '0.4rem',
   },
-}
-*/
+  newNote: {
+    marginTop: '2rem',
+    position: 'relative',
+    width: '4rem',
+  },
+  newNoteImg: {
+    position: 'absolute',
+    left: '0',
+    width: '100%',
+    transition: 'opacity 0.25s ease-in-out',
+  },
+  newNoteImgHover: {
+    ':hover': {
+      opacity: 0,
+    },
+  },
+  signOut: {
+    position: 'absolute',
+    bottom: '1rem',
+  },
+  button: {
+    backgroundColor: 'transparent',
+    border: '0',
+    color: '#008bf8',
+    cursor: 'pointer',
+    outline: 'none',
+  },
+  buttonIcon: {
+    fontSize: '2rem',
+  },
+})
+
 export default Sidebar
